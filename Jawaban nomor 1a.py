ed660df0727d22fea10cb6dad481c9dcbf3f95cf
SalsabiilaWijaya <salsabiilawijaya14@gmail.com>
2024-09-24 12:48:29 +0700

    Materi-2-Penyelesaian Soal Fisika Menggunakan Komputasi-Salsabiila Dhiyaa Wijaya
    Nama     : Salsabiila Dhiyaa Wijaya
    NIM       : 1227030031
    Matkul   : Praktikum Fisika Komputasi
    Modul    : Modul 2_Penyelesaian Soal Fisika Menggunakan Komputasi

---
+++ Jawaban nomor 1a.py
@@ -0,0 +1,5 @@
+Python 3.11.0 (main, Oct 24 2022, 18:26:48) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:\Users\USER\OneDrive\Dokumen\Dunianya Salsa Xixi\KULIAH - UIN SGD BANDUNG\Tugas Kuli-ah\Semester 5\PRAKFISKOM\Modul 2\Nomor 1a.py
+Jarak fokus lensa (f) adalah: 19.493670886075947 cm
---
+++ Jawaban nomor 1b.py
@@ -0,0 +1,9 @@
+Python 3.11.0 (main, Oct 24 2022, 18:26:48) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+================ RESTART: C:\Users\USER\Downloads\TUGAS NO 1b.py ===============
+
+Untuk alpha = 45° dan v0 = 10 m/s:
+Jarak Horizontal Maksimum = 5.1020408163265305 m
+Tinggi Vertikal Maksimum = 1.4430750636460152 m
+Waktu Mencapai Jarak Horizontal Maksimum = 1.4430750636460152 s
